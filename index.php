496a4accd4b2a07249896ba75485bd67655e92e1
Boubacar2601 <boubacardieng85@gmail.com>
2022-02-09 16:34:23 +0100

    ajout une liste

--- index.php
+++ index.php
@@ -97,6 +97,38 @@
 
   <main>
     <h1 class="display-4 text-center my-4">Evalaution Git Abdellah</h1>
+<!-- On tables -->
+<table class="table-primary">...</table>
+<table class="table-secondary">...</table>
+<table class="table-success">...</table>
+<table class="table-danger">...</table>
+<table class="table-warning">...</table>
+<table class="table-info">...</table>
+<table class="table-light">...</table>
+<table class="table-dark">...</table>
+
+<!-- On rows -->
+<tr class="table-primary">...</tr>
+<tr class="table-secondary">...</tr>
+<tr class="table-success">...</tr>
+<tr class="table-danger">...</tr>
+<tr class="table-warning">...</tr>
+<tr class="table-info">...</tr>
+<tr class="table-light">...</tr>
+<tr class="table-dark">...</tr>
+
+<!-- On cells (`td` or `th`) -->
+<tr>
+  <td class="table-primary">...</td>
+  <td class="table-secondary">...</td>
+  <td class="table-success">...</td>
+  <td class="table-danger">...</td>
+  <td class="table-warning">...</td>
+  <td class="table-info">...</td>
+  <td class="table-light">...</td>
+  <td class="table-dark">...</td>
+</tr>
+
     <div class="container">
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eos reprehenderit nulla facilis nisi ad perspiciatis saepe molestiae quas beatae, at tempore veritatis sit, non magni dolor est, accusamus quidem.</p>
 
