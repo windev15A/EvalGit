300022784342f83018f1c861a0d61205e588a940
windev15A <abdellahwindev@gmail.com>
2022-02-09 13:38:56 +0100

    dev offcanevas bootstrap

--- index.php
+++ index.php
@@ -36,6 +36,13 @@
             <li class="nav-item">
               <a class="nav-link" href="#">A propos</a>
             </li>
+
+            <li class="nav-item">
+
+              <a class="nav-link" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                Information
+              </a>
+            </li>
           </ul>
           <form>
             <input class="form-control" type="text" placeholder="Rechercher" aria-label="Search">
@@ -43,6 +50,22 @@
         </div>
       </div>
     </nav>
+
+
+
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <div>
+          Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+        </div>
+        
+      </div>
+    </div>
   </header>
 
   <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
@@ -85,15 +108,15 @@
     </div>
   </main>
 
-  
+
   <footer class="text-center text-lg-start bg-dark text-white">
-    
+
     <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-     
+
       <div class="me-5 d-none d-lg-block">
         <span>Get connected with us on social networks:</span>
       </div>
-      
+
       <div>
         <a href="" class="me-4 text-reset">
           <i class="fab fa-facebook-f"></i>
@@ -114,16 +137,16 @@
           <i class="fab fa-github"></i>
         </a>
       </div>
-     
+
     </section>
-    
+
     <section class="">
       <div class="container text-center text-md-start mt-5">
-       
+
         <div class="row mt-3">
-         
+
           <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            
+
             <h6 class="text-uppercase fw-bold mb-4">
               <i class="fas fa-gem me-3"></i>Company name
             </h6>
