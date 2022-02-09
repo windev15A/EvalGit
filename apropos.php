7959c344119149f8f529b6d980b90057737b83ea
windev15A <abdellahwindev@gmail.com>
2022-02-09 15:05:57 +0100

    Création de la page about +nav

--- apropos.php
+++ apropos.php
@@ -34,7 +34,7 @@
               <a class="nav-link" href="contact.php">Contact</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="apropos.php">A propos</a>
+              <a class="nav-link" href="#">A propos</a>
             </li>
 
             <li class="nav-item">
@@ -68,47 +68,14 @@
     </div>
   </header>
 
-  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="./img/vet1.jpg" class="d-block w-100" alt="./img/vet1.jpg">
-      </div>
-      <div class="carousel-item">
-        <img src="./img/vet2.jpg" class="d-block w-100" alt="./img/vet1.jpg">
-      </div>
-      <div class="carousel-item">
-        <img src="./img/vet3.jpg" class="d-block w-100" alt="./img/vet1.jpg">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-
-  <main>
-    <h1 class="display-4 text-center my-4">Evalaution Git Abdellah</h1>
-    <div class="container">
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eos reprehenderit nulla facilis nisi ad perspiciatis saepe molestiae quas beatae, at tempore veritatis sit, non magni dolor est, accusamus quidem.</p>
-
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eos reprehenderit nulla facilis nisi ad perspiciatis saepe molestiae quas beatae, at tempore veritatis sit, non magni dolor est, accusamus quidem.</p>
-
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eos reprehenderit nulla facilis nisi ad perspiciatis saepe molestiae quas beatae, at tempore veritatis sit, non magni dolor est, accusamus quidem.</p>
-
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eos reprehenderit nulla facilis nisi ad perspiciatis saepe molestiae quas beatae, at tempore veritatis sit, non magni dolor est, accusamus quidem.</p>
-    </div>
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading">A propos GIT .</h1>
+    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <p class="lead">
+      <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+    </p>
   </main>
-
-
+  
   <footer class="text-center text-lg-start bg-dark text-white">
 
     <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
