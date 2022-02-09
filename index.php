ff10f257d4c5aa522d9cb885620d2ddbb07828b7
windev15A <abdellahwindev@gmail.com>
2022-02-09 16:32:37 +0100

    add form

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
         <div>
           Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
         </div>
-        
+
       </div>
     </div>
   </header>
@@ -97,6 +97,22 @@
 
   <main>
     <h1 class="display-4 text-center my-4">Evalaution Git Abdellah</h1>
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
     <div class="container">
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eos reprehenderit nulla facilis nisi ad perspiciatis saepe molestiae quas beatae, at tempore veritatis sit, non magni dolor est, accusamus quidem.</p>
 
